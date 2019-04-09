51df2bd7d2440dba00f4148189cb5898d83ad034
Cactice <14835424+Cactice@users.noreply.github.com>
2019-04-09 09:19:41 +0900

    Init Xcode

---
+++ webview-getusermedia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  webview-getusermedia
+//
+//  Created by Cactice on 4/9/19.
+//  Copyright © 2019 Cactice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
