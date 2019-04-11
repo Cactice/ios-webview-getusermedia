a73c909f32ae9ebdfaafe096398d0fe7dfe1782e
Cactice <14835424+Cactice@users.noreply.github.com>
2019-04-11 09:15:50 +0900

    Add Pods

---
+++ AppRTCViewController.h
@@ -0,0 +1,16 @@
+//
+//  AppRTCViewController.h
+//  webview-getusermedia
+//
+//  Created by Cactice on 4/9/19.
+//  Copyright © 2019 Cactice. All rights reserved.
+//
+
+#ifndef AppRTCViewController_h
+#define AppRTCViewController_h
+
+@interface MyViewController : UIViewController
+@interface ARTCVideoChatViewController : UIViewController <ARDAppClientDelegate, RTCEAGLVideoViewDelegate>
+
+
+#endif /* AppRTCViewController_h */
---
+++ webview-getusermedia/AppRTCViewController.m
@@ -0,0 +1,17 @@
+//
+//  AppRTCViewController.m
+//  webview-getusermedia
+//
+//  Created by Cactice on 4/9/19.
+//  Copyright © 2019 Cactice. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <libjingle_peerconnection/RTCEAGLVideoView.h>
+#import <AppRTC/ARDAppClient.h>
+#import "RTCPeerConnectionFactory.h"
+
+
+
+   [RTCPeerConnectionFactory initializeSSL];
+   [RTCPeerConnectionFactory deinitializeSSL];
